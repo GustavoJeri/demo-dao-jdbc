57b3a1d475654db392cc5e875f8f77e520f611f4
GustavoJeri <gustavo1italo@gmail.com>
2025-10-06 13:13:52 -0300

    Seller entity class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,20 @@
 package org.example;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main {
     static void main() {
-
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
         Department obj =  new Department(1, "Books");
+        Seller seller = new Seller(1, "João", "joao@gmail.com", new Date(), 1500.5, obj);
 
-        System.out.println(obj);
+        System.out.println(seller);
+
 
     }
 }
